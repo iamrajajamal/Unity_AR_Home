fe2420156b65ba59f9a8c035b56b302989703a80
iamrajajamal <martian.hacked@gmail.com>
2019-03-29 18:16:05 +0500

    Add GameController for scene management in app

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void ChangeScene(string name)
+    {
+        SceneManager.LoadSceneAsync(name);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
