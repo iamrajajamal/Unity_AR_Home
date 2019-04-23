77bdcab80ebc128218d83c8ba47ac55c41f3ab44
Raja Jamal Parvez <rajajamalparvez@gmail.com>
2019-04-23 15:57:49 +0500

    Added House Controller and lights in House1 scene

---
+++ Assets/Scripts/HouseScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HouseScript : MonoBehaviour
+{
+    public GameObject theRoof;
+
+    public GameObject[] secondFloor;
+
+    public GameObject[] theLights;
+
+    public GameObject directionalLight;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
